4602f10fb38c020598a867f05d307ae032e18fe9
chhotu marandi <cmarandi29@gmail.com>
2023-05-09 00:45:20 +0530

    add html layout

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-const correctNumber = Math.floor(Math.random() * (10-1+1) + 1);
-let userGuess;
-let attempts = 0;
-let winningPoints = 10;
+// let userGuess;
+// let attempts = 0;
+// let winningPoints = 10;
 
-// console.log(correctNumber)
+// const correctNumber = Math.floor(Math.random() * (10-1+1) + 1);
 
-const guessNumber = () => {
-  do {
-    userGuess = prompt("guess number");
-    // console.log(correctNumber);
-    // console.log(userGuess);
-    if (correctNumber < userGuess) {
-      alert("number is less than entered number");
-    } else if (correctNumber > userGuess) {
-      alert("number is greater than entered number ");
-    } else {
-      alert("congratulation you are correct");
-    }
+// const guessNumber = () => {
+//   do {
+//     userGuess = prompt("guess number");
+//     // console.log(correctNumber);
+//     // console.log(userGuess);
+//     if (correctNumber < userGuess) {
+//       alert("number is less than entered number");
+//     } else if (correctNumber > userGuess) {
+//       alert("number is greater than entered number ");
+//     } else {
+//       alert("congratulation you are correct");
+//     }
 
-    attempts++;
+//     attempts++;
     
-  } while (userGuess != correctNumber);
+//   } while (userGuess != correctNumber);
 
-  alert(`you take ${attempts} attempts`);
-  const guessPoints = winningPoints - attempts;
-  alert(`Your Points - ${guessPoints}`);
+//   alert(`you take ${attempts} attempts`);
+//   const guessPoints = winningPoints - attempts;
+//   alert(`Your Points - ${guessPoints}`);
 
-}
+// }
 
-guessNumber();
-// console.log(randomNumber)
+// guessNumber();

fd3ca60fb376ccc695690cba6767b8a5ac11bc8f
chhotu marandi <cmarandi29@gmail.com>
2023-05-09 00:04:43 +0530

    add how many attempts taken by user

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-const randomNumber = Math.floor(Math.random() * (10-1+1) + 1);
+const correctNumber = Math.floor(Math.random() * (10-1+1) + 1);
 let userGuess;
+let attempts = 0;
 
-console.log(randomNumber)
-do {
-  userGuess = prompt("guess number");
-  console.log(randomNumber);
-  console.log(userGuess)
-} while (userGuess != randomNumber)
+// console.log(correctNumber)
 
+const guessNumber = () => {
+  do {
+    userGuess = prompt("guess number");
+    // console.log(correctNumber);
+    // console.log(userGuess);
+    if (correctNumber < userGuess) {
+      alert("number is less than entered number");
+    } else if (correctNumber > userGuess) {
+      alert("number is greater than entered number ");
+    } else {
+      alert("congratulation you are correct");
+    }
 
+    attempts++;
+    
+  } while (userGuess != correctNumber);
+
+  alert(attempts + "attempts");
+
+}
+
+guessNumber();
 // console.log(randomNumber)

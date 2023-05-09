f424f5a235a8b0b443c71415e5995147611c9885
chhotu marandi <cmarandi29@gmail.com>
2023-05-09 16:51:49 +0530

    added query selector on display

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-// let userGuess;
-// let attempts = 0;
-// let winningPoints = 10;
+const displayPoints = document.querySelector(".points-container");
 
-// const correctNumber = Math.floor(Math.random() * (10-1+1) + 1);
+const displayAttempts = document.querySelector(".attempts-container");
 
-// const guessNumber = () => {
-//   do {
-//     userGuess = prompt("guess number");
-//     // console.log(correctNumber);
-//     // console.log(userGuess);
-//     if (correctNumber < userGuess) {
-//       alert("number is less than entered number");
-//     } else if (correctNumber > userGuess) {
-//       alert("number is greater than entered number ");
-//     } else {
-//       alert("congratulation you are correct");
-//     }
+const mainDisplay = document.querySelector(".mainDisplay");
 
-//     attempts++;
-    
-//   } while (userGuess != correctNumber);
+console.log((mainDisplay.innerHTML = "heio"));
 
-//   alert(`you take ${attempts} attempts`);
-//   const guessPoints = winningPoints - attempts;
-//   alert(`Your Points - ${guessPoints}`);
+console.log((displayAttempts.textContent = "hi"));
+console.log((displayPoints.textContent = "hi"));
 
-// }
+let userGuess;
+let attempts = 0;
+let winningPoints = 10;
+
+const correctNumber = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+
+const guessNumber = () => {
+  do {
+    userGuess = prompt("guess number");
+    // console.log(correctNumber);
+    // console.log(userGuess);
+    if (correctNumber < userGuess) {
+      alert("number is less than entered number");
+    } else if (correctNumber > userGuess) {
+      alert("number is greater than entered number ");
+    } else {
+      alert("congratulation you are correct");
+    }
+
+    attempts++;
+  } while (userGuess != correctNumber);
+
+  alert(`you take ${attempts} attempts`);
+  const guessPoints = winningPoints - attempts;
+  alert(`Your Points - ${guessPoints}`);
+};
 
 // guessNumber();

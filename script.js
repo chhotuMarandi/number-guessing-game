958b3ed9a76a483241ac4fe0f34d5b7dc8c8e498
chhotu marandi <cmarandi29@gmail.com>
2023-05-10 11:15:04 +0530

    added gif files an js

--- script.js
+++ script.js
@@ -2,26 +2,31 @@ const displayPoints = document.querySelector(".points-container");
 
 const displayAttempts = document.querySelector(".attempts-container");
 
+const startBtn = document.getElementById("start");
+console.log(startBtn);
+
 const mainDisplay = document.querySelector(".mainDisplay");
 
-console.log((mainDisplay.innerHTML = "heio"));
+// console.log((mainDisplay.innerHTML = "heio"));
 
-console.log((displayAttempts.textContent = "hi"));
-console.log((displayPoints.textContent = "hi"));
+// console.log((displayAttempts.textContent = "hi"));
+// console.log((displayPoints.textContent = "hi"));
 
 let userGuess;
 let attempts = 0;
 let winningPoints = 10;
 
-const correctNumber = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+// const correctNumber = Math.floor(Math.random() * (10 - 1 + 1) + 1);
 
 const guessNumber = () => {
+  const correctNumber = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+
   do {
     userGuess = prompt("guess number");
     // console.log(correctNumber);
     // console.log(userGuess);
     if (correctNumber < userGuess) {
-      alert("number is less than entered number");
+      mainDisplay.innerHTML = "number is less than entered number";
     } else if (correctNumber > userGuess) {
       alert("number is greater than entered number ");
     } else {
@@ -36,4 +41,5 @@ const guessNumber = () => {
   alert(`Your Points - ${guessPoints}`);
 };
 
+startBtn.addEventListener("click", guessNumber);
 // guessNumber();

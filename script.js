addf3c33ae4b0ebdaf0ce1b3fda7c1900b90766d
chhotu marandi <cmarandi29@gmail.com>
2023-05-18 13:27:38 +0530

    added Comments

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Selctors
+
 const displayPoints = document.querySelector(".points-container");
 
 const displayAttempts = document.querySelector(".attempts-container");
@@ -5,12 +7,10 @@ const displayAttempts = document.querySelector(".attempts-container");
 let attemptesBox = document.querySelector(".attempts-container");
 
 const pointsBox = document.querySelector(".points-container");
-// console.log((pointsBox.innerHTML = " hi"));
 
 const reset = document.querySelector("#reset");
 
 const buttons = document.querySelectorAll(".buttons");
-// console.log(buttons);
 
 const showText = document.querySelector("#showText");
 
@@ -20,8 +20,7 @@ showText.innerHTML = "guess number";
 
 let isRunning = true;
 
-// let userGuess;
-// let userGuessedNumber;
+//Traverse through All Buttons
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -40,6 +39,8 @@ buttons.forEach((btn) => {
   });
 });
 
+//Guess Number Function , Points and Attempts Feature
+
 let attempts = 0;
 let winnigPoints = 10;
 
@@ -57,34 +58,3 @@ const guessNumber = (correctNumber, userGuessedNumber) => {
     pointsBox.innerHTML = earnPoints;
   }
 };
-
-// console.log(attempts);
-// // let userGuess;
-// let attempts = 0;
-// let winningPoints = 10;
-
-// const guessNumber = () => {
-//   const correctNumber = Math.floor(Math.random() * (10 - 1 + 1) + 1);
-
-//   do {
-//     userGuess = prompt("guess number");
-//     // console.log(correctNumber);
-//     // console.log(userGuess);
-//     if (correctNumber < userGuess) {
-//       mainDisplay.innerHTML = "number is less than entered number";
-//     } else if (correctNumber > userGuess) {
-//       mainDisplay.innerHTML = "number is greater than entered number ";
-//     } else {
-//       mainDisplay.innerHTML = "congratulation you are correct";
-//     }
-
-//     attempts++;
-//   } while (userGuess != correctNumber);
-
-//   alert(`you take ${attempts} attempts`);
-//   const guessPoints = winningPoints - attempts;
-//   alert(`Your Points - ${guessPoints}`);
-// };
-
-// startBtn.addEventListener("click", guessNumber);
-// guessNumber();
